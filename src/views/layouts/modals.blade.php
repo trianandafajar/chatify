a20de1d5dfa4ae190df9c2810f427695c90e541e
trianandafajar <mail.trianandafajar@gmail.com>
2025-01-02 12:01:00 +0700

    Coba tebak ini apa

--- src/views/layouts/modals.blade.php
+++ src/views/layouts/modals.blade.php
@@ -1,3 +1,16 @@
+{{-- ---------------------- Image modal box ---------------------- --}}
+<div id="imageModalBox" class="imageModal">
+    <span class="imageModal-close">&times;</span>
+    <img class="imageModal-content" id="imageModalBoxSrc">
+</div>
+
+{{-- ---------------------- Delete Modal ---------------------- --}}
+<div class="app-modal" data-name="delete">
+    <div class="app-modal-container">
+        <div class="app-modal-card" data-name="delete" data-modal="0">
+            <div class="app-modal-header">Are you sure you want to delete this?</div>
+            <div class="app-modal-body">You can not undo this action</div>
+            <div class="app-modal-footer">
                 <a href="javascript:void(0)" class="app-btn cancel">Cancel</a>
                 <a href="javascript:void(0)" class="app-btn a-btn-danger delete">Delete</a>
             </div>
