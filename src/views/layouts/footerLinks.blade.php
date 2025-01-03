2baebc7a05b211366113eaf5c127c0386d3aba90
trianandafajar <mail.trianandafajar@gmail.com>
2025-01-03 12:00:00 +0700

    menyala abangku

--- src/views/layouts/footerLinks.blade.php
+++ src/views/layouts/footerLinks.blade.php
@@ -1,6 +1,3 @@
-<script src="https://js.pusher.com/7.0.3/pusher.min.js"></script>
-<script >
-  // Enable pusher logging - don't include this in production
   Pusher.logToConsole = true;
 
   var pusher = new Pusher("{{ config('chatify.pusher.key') }}", {
