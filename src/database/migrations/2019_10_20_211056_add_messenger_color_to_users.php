299ce1256cfcab63e315c672e687d5e5b97183f1
trianandafajar <mail.trianandafajar@gmail.com>
2025-01-04 12:00:00 +0700

    menyala abangku

--- src/database/migrations/2019_10_20_211056_add_messenger_color_to_users.php
+++ src/database/migrations/2019_10_20_211056_add_messenger_color_to_users.php
@@ -1,8 +1,3 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class AddMessengerColorToUsers extends Migration
 {
