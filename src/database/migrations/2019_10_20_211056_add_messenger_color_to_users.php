7b8f16b3faaebac9c0d65312d871366a89e43b60
trianandafajar <mail.trianandafajar@gmail.com>
2025-01-04 12:01:00 +0700

    Coba tebak ini apa

--- src/database/migrations/2019_10_20_211056_add_messenger_color_to_users.php
+++ src/database/migrations/2019_10_20_211056_add_messenger_color_to_users.php
@@ -1,3 +1,8 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddMessengerColorToUsers extends Migration
 {
