7fda485d4c0164be88ad55ad6117432d2b751bf7
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-03 09:28:37 +0700

    Update View messenger and format

--- src/views/layouts/messengerColor.blade.php
+++ src/views/layouts/messengerColor.blade.php
@@ -1,73 +1,76 @@
 <style>
-/* NProgress background */
-#nprogress .bar{
-	background: {{ $messengerColor }} !important;
-}
-#nprogress .peg {
-    box-shadow: 0 0 10px {{ $messengerColor }}, 0 0 5px {{ $messengerColor }} !important;
-}
-#nprogress .spinner-icon {
-  border-top-color: {{ $messengerColor }} !important;
-  border-left-color: {{ $messengerColor }} !important;
-}
+    /* NProgress background */
+    #nprogress .bar {
+        background: {{ $messengerColor }} !important;
+    }
 
-.m-header svg{
-    color: {{ $messengerColor }};
-}
+    #nprogress .peg {
+        box-shadow: 0 0 10px {{ $messengerColor }}, 0 0 5px {{ $messengerColor }} !important;
+    }
 
-.m-list-active,
-.m-list-active:hover,
-.m-list-active:focus{
-	background: {{ $messengerColor }};
-}
+    #nprogress .spinner-icon {
+        border-top-color: {{ $messengerColor }} !important;
+        border-left-color: {{ $messengerColor }} !important;
+    }
 
-.m-list-active b{
-	background: #fff !important;
-	color: {{ $messengerColor }} !important;
-}
+    .m-header svg {
+        color: {{ $messengerColor }};
+    }
 
-.messenger-list-item td b{
-    background: {{ $messengerColor }};
-}
+    .m-list-active,
+    .m-list-active:hover,
+    .m-list-active:focus {
+        background: {{ $messengerColor }};
+    }
 
-.messenger-infoView nav a{
-    color: {{ $messengerColor }};
-}
+    .m-list-active b {
+        background: #fff !important;
+        color: {{ $messengerColor }} !important;
+    }
 
-.messenger-infoView-btns a.default{
-	color: {{ $messengerColor }};
-}
+    .messenger-list-item td b {
+        background: {{ $messengerColor }};
+    }
 
-.mc-sender p{
-  background: {{ $messengerColor }};
-}
+    .messenger-infoView nav a {
+        color: {{ $messengerColor }};
+    }
 
-.messenger-sendCard button svg{
-    color: {{ $messengerColor }};
-}
+    .messenger-infoView-btns a.default {
+        color: {{ $messengerColor }};
+    }
 
-.messenger-listView-tabs a,
-.messenger-listView-tabs a:hover,
-.messenger-listView-tabs a:focus{
-    color: {{ $messengerColor }};
-}
+    .mc-sender p {
+        background: {{ $messengerColor }};
+    }
 
-.active-tab{
-	border-bottom: 2px solid {{ $messengerColor }};
-}
+    .messenger-sendCard button svg {
+        color: {{ $messengerColor }};
+    }
 
-.lastMessageIndicator{
-    color: {{ $messengerColor }} !important;
-}
+    .messenger-listView-tabs a,
+    .messenger-listView-tabs a:hover,
+    .messenger-listView-tabs a:focus {
+        color: {{ $messengerColor }};
+    }
 
-.messenger-favorites div.avatar{
-    box-shadow: 0px 0px 0px 2px {{ $messengerColor }};
-}
+    .active-tab {
+        border-bottom: 2px solid {{ $messengerColor }};
+    }
 
-.dark-mode-switch{
-    color: {{ $messengerColor }};
-}
-.m-list-active .activeStatus{
-    border-color: {{ $messengerColor }} !important;
-}
+    .lastMessageIndicator {
+        color: {{ $messengerColor }} !important;
+    }
+
+    .messenger-favorites div.avatar {
+        box-shadow: 0px 0px 0px 2px {{ $messengerColor }};
+    }
+
+    .dark-mode-switch {
+        color: {{ $messengerColor }};
+    }
+
+    .m-list-active .activeStatus {
+        border-color: {{ $messengerColor }} !important;
+    }
 </style>
