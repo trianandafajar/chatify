fe92853e381cd0b8f939e35d8cd597338a44386a
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-07 09:16:11 +0700

    Update Info

--- src/views/layouts/info.blade.php
+++ src/views/layouts/info.blade.php
@@ -1,12 +1,32 @@
-{{-- user info and avatar --}}
-<div class="avatar av-l"></div>
-<p class="info-name">{{ config('chatify.name') }}</p>
-<div class="messenger-infoView-btns">
-    {{-- <a href="#" class="default"><i class="fas fa-camera"></i> default</a> --}}
-    <a href="#" class="danger delete-conversation"><i class="fas fa-trash-alt"></i> Delete Conversation</a>
-</div>
-{{-- shared photos --}}
-<div class="messenger-infoView-shared">
-    <p class="messenger-title">Share photos</p>
-    <div class="shared-photos-list"></div>
+{{-- -------------------- User Info and Avatar -------------------- --}}
+<div class="messenger-infoView">
+    {{-- Avatar dan Nama --}}
+    <div class="messenger-infoView-header">
+        <div class="avatar av-l" style="background-image: url('{{ asset('/storage/' . config('chatify.user_avatar.folder') . '/' . $user->avatar) }}');"></div>
+        <p class="info-name">{{ config('chatify.name') }}</p>
+    </div>
+
+    {{-- Tombol Aksi --}}
+    <div class="messenger-infoView-btns">
+        {{-- Tombol default (jika ingin diaktifkan bisa buka komentar) --}}
+        {{-- <a href="#" class="default"><i class="fas fa-camera"></i> Default</a> --}}
+        <a href="javascript:void(0)" class="danger delete-conversation">
+            <i class="fas fa-trash-alt"></i> Delete Conversation
+        </a>
+    </div>
+
+    {{-- Shared Photos --}}
+    <div class="messenger-infoView-shared">
+        <p class="messenger-title">Shared Photos</p>
+        <div class="shared-photos-list">
+            @if(isset($sharedPhotos) && count($sharedPhotos) > 0)
+                @foreach($sharedPhotos as $photo)
+                    <div class="shared-photo chat-image"
+                         style="background-image: url('{{ asset($photo->url) }}');"></div>
+                @endforeach
+            @else
+                <p style="text-align: center; color: #aaa;">No photos shared yet.</p>
+            @endif
+        </div>
+    </div>
 </div>
