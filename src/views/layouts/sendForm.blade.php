096c59c7ac43f4c9f21878f0010d05f0bdfd9665
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-07 09:16:38 +0700

    Update Send Form

--- src/views/layouts/sendForm.blade.php
+++ src/views/layouts/sendForm.blade.php
@@ -1,8 +1,17 @@
 <div class="messenger-sendCard">
     <form id="message-form" method="POST" action="{{ route('send.message') }}" enctype="multipart/form-data">
         @csrf
-        <label><span class="fas fa-paperclip"></span><input disabled='disabled' type="file" class="upload-attachment" name="file" accept="image/*, .txt, .rar, .zip" /></label>
-        <textarea readonly='readonly' name="message" class="m-send app-scroll" placeholder="Type a message.."></textarea>
-        <button disabled='disabled'><span class="fas fa-paper-plane"></span></button>
+
+        <!-- Upload attachment -->
+        <label>
+            <span class="fas fa-paperclip"></span>
+            <input disabled type="file" class="upload-attachment" name="file" accept="image/*, .txt, .rar, .zip" />
+        </label>
+
+        <!-- Message box -->
+        <textarea readonly name="message" class="m-send app-scroll" placeholder="Type a message.."></textarea>
+
+        <!-- Send button -->
+        <button disabled><span class="fas fa-paper-plane"></span></button>
     </form>
 </div>
