11d8c1dc3c4667a445a67f797c6ba65c321f5ecc
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-07 09:16:01 +0700

    Uodate Favorite

--- src/views/layouts/favorite.blade.php
+++ src/views/layouts/favorite.blade.php
@@ -1,6 +1,12 @@
 <div class="favorite-list-item">
-    <div data-id="{{ $user->id }}" data-action="0" class="avatar av-m" 
-        style="background-image: url('{{ asset('/storage/'.config('chatify.user_avatar.folder').'/'.$user->avatar) }}');">
+    <div data-id="{{ $user->id }}" data-action="0" class="avatar av-m"
+        style="
+            background-image: url('{{ $user->avatar
+                ? asset('/storage/' . config('chatify.user_avatar.folder') . '/' . $user->avatar)
+                : asset('images/default-avatar.png') }}');
+        ">
     </div>
-    <p>{{ strlen($user->name) > 5 ? substr($user->name,0,6).'..' : $user->name }}</p>
+    <p title="{{ $user->name }}">
+        {{ strlen($user->name) > 5 ? substr($user->name, 0, 6) . '..' : $user->name }}
+    </p>
 </div>
