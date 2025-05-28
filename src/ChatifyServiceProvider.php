d3e7b1d0d719b148e35f39d64e9d423fd20b2323
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-28 10:45:13 +0700

    Update Chatify Service

--- src/ChatifyServiceProvider.php
+++ src/ChatifyServiceProvider.php
@@ -17,7 +17,7 @@ class ChatifyServiceProvider extends ServiceProvider
     public function register()
     {
         app()->bind('ChatifyMessenger', function () {
-            return new \Chatify\ChatifyMessenger;
+            return new ChatifyMessenger;
         });
     }
 
@@ -28,7 +28,7 @@ class ChatifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Load Views, Migrations and Routes
+        // Load Views, Migrations, Routes
         $this->loadViewsFrom(__DIR__ . '/views', 'Chatify');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->loadRoutes();
@@ -40,58 +40,55 @@ class ChatifyServiceProvider extends ServiceProvider
             ]);
             $this->setPublishes();
         }
-
     }
 
     /**
-     * Publishing the files that the user may override.
+     * Set the files that can be published by users.
      *
      * @return void
      */
     protected function setPublishes()
     {
-        // Load user's avatar folder from package's config
-        $userAvatarFolder = json_decode(json_encode(include(__DIR__.'/config/chatify.php')))->user_avatar->folder;
+        // Load avatar folder from package config
+        $config = include(__DIR__ . '/config/chatify.php');
+        $userAvatarFolder = $config['user_avatar']['folder'];
 
-        // Config
+        // Publish config
         $this->publishes([
-            __DIR__ . '/config/chatify.php' => config_path('chatify.php')
+            __DIR__ . '/config/chatify.php' => config_path('chatify.php'),
         ], 'chatify-config');
 
-        // Migrations
+        // Publish migrations
         $this->publishes([
-            __DIR__ . '/database/migrations/' => database_path('migrations')
+            __DIR__ . '/database/migrations/' => database_path('migrations'),
         ], 'chatify-migrations');
 
-        // Models
-        $isV8 = explode('.',app()->version())[0] >= 8;
+        // Publish models
+        $isV8 = explode('.', app()->version())[0] >= 8;
         $this->publishes([
-            __DIR__ . '/Models' => app_path($isV8 ? 'Models' : '')
+            __DIR__ . '/Models' => app_path($isV8 ? 'Models' : ''),
         ], 'chatify-models');
 
-        // Controllers
+        // Publish controllers
         $this->publishes([
-            __DIR__ . '/Http/Controllers' => app_path('Http/Controllers/vendor/Chatify')
+            __DIR__ . '/Http/Controllers' => app_path('Http/Controllers/vendor/Chatify'),
         ], 'chatify-controllers');
 
-        // Views
+        // Publish views
         $this->publishes([
-            __DIR__ . '/views' => resource_path('views/vendor/Chatify')
+            __DIR__ . '/views' => resource_path('views/vendor/Chatify'),
         ], 'chatify-views');
 
-        // Assets
+        // Publish assets (CSS, JS, Images)
         $this->publishes([
-            // CSS
             __DIR__ . '/assets/css' => public_path('css/chatify'),
-            // JavaScript
             __DIR__ . '/assets/js' => public_path('js/chatify'),
-            // Images
-            __DIR__ . '/assets/imgs' => storage_path('app/public/' . $userAvatarFolder),
+            __DIR__ . '/assets/imgs' => storage_path("app/public/{$userAvatarFolder}"),
         ], 'chatify-assets');
     }
 
     /**
-     * Group the routes and set up configurations to load them.
+     * Load web routes for Chatify.
      *
      * @return void
      */
@@ -103,16 +100,16 @@ class ChatifyServiceProvider extends ServiceProvider
     }
 
     /**
-     * Routes configurations.
+     * Routes configuration options.
      *
      * @return array
      */
     private function routesConfigurations()
     {
         return [
-            'prefix' => config('chatify.routes.prefix'),
-            'namespace' =>  config('chatify.routes.namespace'),
-            'middleware' => config('chatify.routes.middleware'),
+            'prefix' => config('chatify.routes.prefix', 'chatify'),
+            'namespace' => config('chatify.routes.namespace', 'Chatify\Http\Controllers'),
+            'middleware' => config('chatify.routes.middleware', ['web', 'auth']),
         ];
     }
 }
