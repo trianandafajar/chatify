944df6d6ee5d1ad77f2753f760def687bb1ffe80
trianandafajar <mail.trianandafajar@gmail.com>
2025-07-22 11:01:52 +0700

    Update Chat Message

--- src/Models/ChMessage.php
+++ src/Models/ChMessage.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChMessage extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'type', 'from_id', 'to_id', 'body', 'attachment', 'seen', 'created_at', 'updated_at'
+    ];
 }
