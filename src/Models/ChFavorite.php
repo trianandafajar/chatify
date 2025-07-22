9dec6fd11fa97b9cbb81dc9e4cdf17e488765f79
trianandafajar <mail.trianandafajar@gmail.com>
2025-07-22 11:01:36 +0700

    Update Chat Favorit

--- src/Models/ChFavorite.php
+++ src/Models/ChFavorite.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChFavorite extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'user_id', 'favorite_id', 'created_at', 'updated_at'
+    ];
 }
